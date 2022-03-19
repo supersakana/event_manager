d5ddd1f65161152c738f6bcfb841cd2cb3d9bb39
supersakana <zacwilliamson@icloud.com>
2022-03-19 12:12:20 -0600

    script now puts personal letter to each individual using gsub/gsub

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,26 +3,13 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
-def clean_zip(zipcode)
+def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts 'EventManager initialized.'
-
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
-contents.each do |row|
-  name = row[:first_name]
-  zip = row[:zipcode]
-  clean_zip(zip)
-  puts "#{name} - #{zip}"
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
     legislators = civic_info.representative_info_by_address(
@@ -31,9 +18,33 @@ contents.each do |row|
       roles: %w[legislatorUpperBody legislatorLowerBody]
     )
     legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(', ')
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+end
 
-  puts "#{name} #{zip} #{legislators}"
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  '../event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('../form_letter.html')
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
+  # puts "#{name} #{zipcode} #{legislators}"
 end
